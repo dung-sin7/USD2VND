0082e88f71c04b441e30c8dc24c018e968201bac
UIT-20521205 <dinhiendung10@gmail.com>
2023-04-01 21:06:22 +0700

    Complete

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace USD2VND;
+class Program
+{
+    static void Main(string[] args)
+    {
+        float rate = 23000f;
+        Console.Write("Input amount of money(USD): ");
+        float usd = float.Parse(Console.ReadLine());
+        Console.WriteLine($"{usd} USD -> {usd * rate} VND");
+    }
+}
